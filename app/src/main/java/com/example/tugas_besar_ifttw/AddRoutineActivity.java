a416c5c41d61b1c3df0c9e69a27982a2b66e771c
EdwardAJ <13517115@std.stei.itb.ac.id>
2020-03-13 15:10:43 +0700

    Add landscape layout

--- app/src/main/java/com/example/tugas_besar_ifttw/AddRoutineActivity.java
+++ app/src/main/java/com/example/tugas_besar_ifttw/AddRoutineActivity.java
@@ -20,32 +20,56 @@ public class AddRoutineActivity extends AppCompatActivity {
     private ViewPager addRoutinePager;
     public Context context;
 
+    private TabLayout tabLayoutTwo;
+    private ViewPager addRoutinePagerTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         int orientation = getResources().getConfiguration().orientation;
+
         if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             setContentView(R.layout.activity_add_routine_land);
+
+            this.tabLayout = findViewById(R.id.tabLayout);
+            this.tabLayoutTwo = findViewById(R.id.tabLayoutTwo);
+
+            // For fragments
+            this.addRoutinePager = findViewById(R.id.viewPager);
+            this.addRoutinePagerTwo = findViewById(R.id.viewPagerTwo);
+
+            // Apply fragment from fragment_news.xml and fragment_timer.xml
+            ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+            adapter.addFragment(new FragmentTimer(), "Timer");
+
+            this.addRoutinePager.setAdapter(adapter);
+            this.tabLayout.setupWithViewPager(addRoutinePager);
+
+            ViewPagerAdapter adapterTwo = new ViewPagerAdapter(getSupportFragmentManager());
+            adapterTwo.addFragment(new FragmentNews(), "News");
+            adapterTwo.addFragment(new FragmentSensor(), "Sensor");
+
+            this.addRoutinePagerTwo.setAdapter(adapterTwo);
+            this.tabLayoutTwo.setupWithViewPager(addRoutinePagerTwo);
+
         } else {
             setContentView(R.layout.activity_add_routine);
-        }
 
+            this.tabLayout = findViewById(R.id.tabLayout);
+
+            // For fragments
+            this.addRoutinePager = findViewById(R.id.viewPager);
+            // Apply fragment from fragment_news.xml and fragment_timer.xml
+            ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+            adapter.addFragment(new FragmentTimer(), "Timer");
+            adapter.addFragment(new FragmentNews(), "News");
+            adapter.addFragment(new FragmentSensor(), "Sensor");
+
+            this.addRoutinePager.setAdapter(adapter);
+            this.tabLayout.setupWithViewPager(addRoutinePager);
+        }
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        this.tabLayout = findViewById(R.id.tabLayout);
-        // For fragments
-        this.addRoutinePager = findViewById(R.id.viewPager);
-
-        // Apply fragment from fragment_news.xml and fragment_timer.xml
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new FragmentTimer(), "Timer");
-        adapter.addFragment(new FragmentNews(), "News");
-        adapter.addFragment(new FragmentSensor(), "Sensor");
-
-        this.addRoutinePager.setAdapter(adapter);
-        this.tabLayout.setupWithViewPager(addRoutinePager);
     }
 }
